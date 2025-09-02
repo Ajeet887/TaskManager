71d3ee156e2fd9b84d20f300920a2d39d0db9d09
Ajeet887 <prajapatajeet22@gmail.com>
2025-09-02 20:44:57 +0530

    update commit

--- Frontend/task-manager/src/components/Cards/UserCard.jsx
+++ Frontend/task-manager/src/components/Cards/UserCard.jsx
@@ -6,7 +6,7 @@ const UserCard = ({ userInfo }) => {
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
           <img
-            src={userInfo}
+            src={userInfo?.profileImageUrl}
             alt={'Avatar'}
             className="w-12 h-12 rounded-full border-2 border-white"
           />
